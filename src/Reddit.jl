efdc1e74dc3eb6c79f0ff1f1eedb496463da5584
Kenneth Berry <rosskberry@gmail.com>
2019-08-14 23:13:53 -0600

    Add function that reads config file
    Add readconfig function, which reads the config.ini file and builds a Session from the configuration settings it contains.

--- src/Reddit.jl
+++ src/Reddit.jl
@@ -4,6 +4,7 @@ using Revise
 using HTTP
 using TimeSeries
 using ConfParser
+using JSON
 
 mutable struct Client
     id::String
@@ -23,7 +24,23 @@ end
 function readconfig()
     conf = ConfParse("config/config.ini")
     parse_conf!(conf)
-    return conf
+    sections = collect(keys(conf._data))
+    names = [s for s in sections if occursin("client", s)]
+    clients = Dict{String, Client}()
+    for name in names
+        id = retrieve(conf, name, "client_id")
+        secret = retrieve(conf, name, "client_secret")
+        password = retrieve(conf, name, "password")
+        username = retrieve(conf, name, "username")
+        if !haskey(clients, name)
+            clients[name] = Client(name, id, secret, password, username)
+        end
+    end
+    default = conf._data["default"]
+    url = default["reddit_url"][1]
+    shorturl = default["short_url"][1]
+    oathurl = default["oauth_url"][1]
+    Session(url, shorturl, oathurl, clients)
 end
 
 end # module
