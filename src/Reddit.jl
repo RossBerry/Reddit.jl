755e60ef378a88613b72df4f143d13c8c5e5e4c0
Kenneth Berry <rosskberry@gmail.com>
2019-08-14 23:12:09 -0600

    Add Client & Session types

--- src/Reddit.jl
+++ src/Reddit.jl
@@ -5,6 +5,21 @@ using HTTP
 using TimeSeries
 using ConfParser
 
+mutable struct Client
+    id::String
+    secret::String
+	user_agent::String
+    password::String
+    username::String
+end
+
+mutable struct Session
+    url::String
+    shorturl::String
+    oath::String
+    clients::Dict{String, Client}
+end
+
 function readconfig()
     conf = ConfParse("config/config.ini")
     parse_conf!(conf)
